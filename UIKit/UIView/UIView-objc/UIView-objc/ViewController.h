d29f72654f9a8f89ee5cff3e63260075005c6d2d
SwordAndTea <xiangweiqaz@gmail.com>
2018-12-11 20:38:27 +0800

    create UIView example project

---
+++ UIKit/UIView/UIView-objc/UIView-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView-objc
+//
+//  Created by 向尉 on 2018/12/11.
+//  Copyright © 2018 SwordAndTea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIKit/UIView/UIView-objc/UIView-objcTests/UIView_objcTests.m
@@ -0,0 +1,37 @@
+//
+//  UIView_objcTests.m
+//  UIView-objcTests
+//
+//  Created by 向尉 on 2018/12/11.
+//  Copyright © 2018 SwordAndTea. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIView_objcTests : XCTestCase
+
+@end
+
+@implementation UIView_objcTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
